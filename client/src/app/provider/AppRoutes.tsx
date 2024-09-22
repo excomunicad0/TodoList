1476503e64b3cb572534be36054f286133a4e89d
Excomunicad0 <e_zhul@mail.ru>
2024-09-22 12:38:31 +0300

    feat/AddMainPage

--- client/src/app/provider/AppRoutes.tsx
+++ client/src/app/provider/AppRoutes.tsx
@@ -3,12 +3,12 @@ import { Route, Routes } from 'react-router-dom';
 import RegistrationPage from '../../page/RegistrationPage/RegistrationPage';
 import AuthorizationPage from '../../page/AuthorizationPage/AuthorizationPage';
 import TasksPage from '../../page/TasksPage/TasksPage';
-
+import MainPage from '../../page/MainPage/MainPage';
 
 function AppRouters(): JSX.Element {
   return (
     <Routes>
-      <Route path='/' />
+      <Route path='/' element={<MainPage />} />
       <Route path='/registration' element={<RegistrationPage />} />
       <Route path='/authorization' element={<AuthorizationPage />} />
       <Route path='/tasks' element={<TasksPage />} />
